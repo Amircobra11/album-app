a201cf9f2260a907db0a303c4c00b792b5f9a770
amir <amir.sisi.cobra11.com@gmail.com>
2025-04-10 20:19:29 +0330

    Configure main App component with routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import Navigation from './components/Navigation';
+import Home from './components/Home';
+import AlbumList from './components/AlbumList';
+import Album from './components/Album';
+import ArtistList from './components/ArtistList';
+import ArtistDetail from './components/ArtistDetail';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Navigation />
+        <main className="container">
+          <Routes>
+            <Route path="/" element={
+              <CSSTransition
+                classNames="page-transition"
+                timeout={300}
+              >
+                <Home />
+              </CSSTransition>
+            } />
+            <Route path="/albums" element={<AlbumList />} />
+            <Route path="/albums/:id" element={<Album />} />
+            <Route path="/artists" element={<ArtistList />} />
+            <Route path="/artists/:id" element={<ArtistDetail />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
