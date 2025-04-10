1b6e0d2fe419c1fcce4fcf6e4a7eb06eab62f450
amir <amir.sisi.cobra11.com@gmail.com>
2025-04-10 16:32:35 +0330

    Implement ViewSets for API endpoints

--- backend/albums/views.py
+++ backend/albums/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Artist, Album, Song
+from .serializers import ArtistSerializer, AlbumSerializer, SongSerializer
 
-# Create your views here.
+
+class ArtistViewSet(viewsets.ModelViewSet):
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
+
+
+class AlbumViewSet(viewsets.ModelViewSet):
+    queryset = Album.objects.all()
+    serializer_class = AlbumSerializer
+
+    def get_queryset(self):
+        queryset = Album.objects.all()
+        artist_id = self.request.query_params.get('artist_id')
+        if artist_id:
+            queryset = queryset.filter(artist_id=artist_id)
+        return queryset
+
+
+class SongViewSet(viewsets.ModelViewSet):
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
+
+    def get_queryset(self):
+        queryset = Song.objects.all()
+        album_id = self.request.query_params.get('album_id')
+        if album_id:
+            queryset = queryset.filter(album_id=album_id)
+        return queryset
