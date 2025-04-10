152ba7cd72d25711f500fe0fd6582a7543982d6c
amir <amir.sisi.cobra11.com@gmail.com>
2025-04-10 16:42:32 +0330

    Add admin for Artist, Album and Song models

--- backend/albums/admin.py
+++ backend/albums/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Artist, Album, Song
 
-# Register your models here.
+
+@admin.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+    search_fields = ('name',)
+
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ('title', 'artist', 'release_date', 'created_at')
+    list_filter = ('artist', 'release_date')
+    search_fields = ('title', 'artist__name')
+
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    list_display = ('track_number', 'title', 'album', 'duration', 'created_at')
+    list_filter = ('album',)
+    search_fields = ('title', 'album__title')
